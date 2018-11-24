9c89a77e0758cf16e4e990caac234ad9a0f401d9
Vildan Hakanaj <vildanhakanaj@trentu.ca>
2018-11-23 20:19:26 -0500

    Something went wrong

---
+++ src/IMemory.java
@@ -0,0 +1,9 @@
+public interface IMemory {
+  int find(int reference);
+  int getEmptySpot();
+  void deleteAll(int id);
+  void printMemory();
+  void insert(Job job, int index);
+  Job get(int index);
+  void updateTime(int index, int clock);
+}
