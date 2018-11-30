65c9e15944d72ea30ba4f346db5eae14d3712c39
Vildan Hakanaj <vildanhakanaj@trentu.ca>
2018-11-30 15:12:37 -0500

    Added a member wise clone to the job class so i can copy the jobs array --> The simulation will run all the files --> added a method to empty the memory --> Added the header for the classes

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
-import java.util.Scanner;
+/*======================================================================================================================
+|   Simulation of a paging system and the LRU and RANDOM algorithm for swaping mechanism
+|
+|   Name:           Main Class
+|
+|   Written by:     Vildan Hakanaj - November 2018
+|
+|   Written for:    COIS 3320 (Prof. Jacques Beland)Lab 3 Trent University Fall 2018.
+|
+|   Purpose:        To simulate how memory management system works. Given files to load in memory.
+|                   It present two different algorithm for decision making on what job to swap and see
+|                   the different output results
+|
+|
+|   usage:          Runs in any java IDE
+|
+|   Subroutines/libraries required:
+|       No Subroutines/libraries utilized outside of Java.util.*
+|
+======================================================================================================================*/
 
 public class Main{
 
-  // TODO: 2018-11-28 [ ] Add a menu so the user can input what algorithm to use
-  // TODO: 2018-11-28 [ ] Add a repetitive loop so the alogrithm runs until user kills it 
   public static void main(String[] args) {
-    Scanner scn = new Scanner(System.in);
-    int choice;
-
-    System.out.println("Enter the line number for the algorithm you wish to execute");
-    System.out.println("Algorithms:\n1.Least Recent Used\n2. Random swap");
-
-    choice = scn.nextInt();
-    Paging paging = new Paging(choice);
+    Paging paging = new Paging();
     paging.runSimulation();
   }
 }
